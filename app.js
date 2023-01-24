176b8b04af45d09e41853872596dd4a3ba8237d2
Vishal  Kacha <vishaldineshbhaikacha@gmail.com>
2023-01-24 09:08:21 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// let theme = document.querySelector('html');
+// if (theme.dataset.theme == "dark") {
+//      document.querySelectorAll(".ionicon")[0].style.color = "black";
+// } else {
+//     console.log("light");
+//     document.querySelectorAll(".ionicon")[0].style.color = "green";
+// }
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('move');
+//         } else {
+//             entry.target.classList.remove('move');
+//         }
+//     });
+// });
+
+// const targets = document.querySelectorAll('.hidden');
+// targets.forEach(target => {
+//     observer.observe(target);
+// });
