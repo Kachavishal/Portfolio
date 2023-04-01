552acea8067af8c04758ddfe930cbb7842dd7dfb
Vishal  Kacha <vishaldineshbhaikacha@gmail.com>
2023-04-01 19:23:37 +0530

    rewrote whole site

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-// let theme = document.querySelector('html');
-// if (theme.dataset.theme == "dark") {
-//      document.querySelectorAll(".ionicon")[0].style.color = "black";
-// } else {
-//     console.log("light");
-//     document.querySelectorAll(".ionicon")[0].style.color = "green";
-// }
 
-// const observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('move');
-//         } else {
-//             entry.target.classList.remove('move');
-//         }
-//     });
-// });
+const button1 = document.getElementById("theme-toggle");
+button1.addEventListener("click", toogleTheme);
+function toogleTheme() {
+    let theme = document.querySelector('html');
+    if (theme.dataset.theme == "dark") {
+        theme.dataset.theme = "light";
+    } else {
+        theme.dataset.theme = "dark";
+    }
+}
 
-// const targets = document.querySelectorAll('.hidden');
-// targets.forEach(target => {
-//     observer.observe(target);
-// });
+
+
