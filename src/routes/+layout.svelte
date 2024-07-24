6165cc54c8b3ee81a6cb4bb03c10e22e6038cece
vishal-kacha <vishaldineshbhaikacha@gmail.com>
2024-07-24 21:09:00 +0530

    svelte new site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="bg-zinc-50 border">
+	<nav class="max-w-screen-xl m-auto p-4 flex justify-between items-center">
+		<h1 class="text-lg">
+			<a href="/"> VISHAL KACHA </a>
+		</h1>
+		<ul class="flex">
+			<li class="px-4"><a href="/resume">Resume</a></li>
+		</ul>
+	</nav>
+</div>
+
+<slot />
